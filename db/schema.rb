c56a7f0a45f114c81bee7e03b9382f396b40ef0b
Joel Azemar <joel.azemar@gmail.com>
2023-08-09 17:56:29 +0200

    Add Seed

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_809_112_019) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_112019) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", null: false
@@ -23,4 +21,5 @@ ActiveRecord::Schema[7.0].define(version: 20_230_809_112_019) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+FactoryBot.create(:user, name: "John", email: "john.doe@acme.com", password: "123456", password_confirmation: "123456")
