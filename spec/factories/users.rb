88e66e5cb3bd60617078abfa1a542ea6a021070a
Joel Azemar <joel.azemar@gmail.com>
2023-08-15 14:47:38 +0200

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@
 
 FactoryBot.define do
   factory :user do
+    id { ArUlid.configuration.generator.generate_id }
     name { FFaker::Name.unique.name }
     email { FFaker::Internet.unique.email }
     username { FFaker::Internet.unique.user_name }
