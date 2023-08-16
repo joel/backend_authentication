68e8970cfa7e5560880bdcdf461d79d641db5a8a
Joel Azemar <joel.azemar@gmail.com>
2023-08-16 13:00:10 +0200

    Add Api V1 Project Create

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -18,7 +18,23 @@ module Api
         render jsonapi: Project.find(params[:id])
       end
 
+      def create
+        project = Project.new(create_project_params)
+        project.user_id = Current.user.id
+
+        if project.save
+          render jsonapi: project, status: :created, location: project
+        else
+          render jsonapi_errors: project.errors, status: :unprocessable_entity
+        end
+      end
+
       private
+
+      # Only allow a list of trusted parameters through.
+      def create_project_params
+        params.require(:project).permit(:name, :id)
+      end
 
       def jsonapi_meta(resources)
         { total: resources.count } if resources.respond_to?(:count)
--- spec/requests/api/v1/projects_spec.rb
+++ spec/requests/api/v1/projects_spec.rb
@@ -16,6 +16,13 @@ RSpec.describe "/api/v1/projects" do
   let(:id) { "01H7YRXCXK0M10W3RC045GW000" }
   let(:name) { "Manhattan" }
   let(:project) { create(:project, id:, name:, user:) }
+  let(:valid_attributes) do
+    attributes_for(:project)
+  end
+
+  let(:invalid_attributes) do
+    valid_attributes.merge({ name: "" })
+  end
 
   before { project }
 
@@ -79,6 +86,88 @@ RSpec.describe "/api/v1/projects" do
           }
         }
       )
+    end
+  end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      subject(:create_project_call) do
+        post api_projects_url, params: { project: valid_attributes }, headers: valid_headers, as: :json
+      end
+
+      let(:created_project) { Project.last }
+
+      it "creates a new Project" do
+        expect do
+          create_project_call
+        end.to change(Project, :count).by(1)
+      end
+
+      it "renders a JSON response with the new project" do
+        create_project_call
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to match(a_string_including("application/vnd.api+json"))
+      end
+
+      it "assigns to the current user" do
+        create_project_call
+        expect(created_project.user).to eq(user)
+      end
+
+      it "renders a valid JSON" do
+        create_project_call
+
+        expect(JSON.parse(response.body)).to eql( # rubocop:disable Rails/ResponseParsedBody
+          {
+            "data" => {
+              "id" => valid_attributes[:id],
+              "type" => "project",
+              "attributes" => {
+                "name" => valid_attributes[:name]
+              }
+            },
+            "links" => {
+              "self" => "http://www.example.com/api/projects"
+            }
+          }
+        )
+      end
+    end
+
+    context "with invalid parameters" do
+      subject(:create_project_call) do
+        post api_projects_url, params: { project: invalid_attributes }, headers: valid_headers, as: :json
+      end
+
+      it "does not create a new Project" do
+        expect do
+          create_project_call
+        end.not_to change(Project, :count)
+      end
+
+      it "renders a JSON response with errors for the new project" do
+        create_project_call
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to match(a_string_including("application/vnd.api+json"))
+      end
+
+      it "renders a valid JSON" do
+        create_project_call
+
+        expect(JSON.parse(response.body)).to eql( # rubocop:disable Rails/ResponseParsedBody
+          {
+            "errors" => [
+              {
+                "status" => "422",
+                "source" => { "pointer" => "/data/attributes/name" },
+                "title" => "Unprocessable Entity",
+                "detail" => "Name can't be blank",
+                "code" => "blank"
+              }
+            ]
+          }
+        )
+      end
     end
   end
 end
